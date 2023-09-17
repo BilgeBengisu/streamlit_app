6582a8314052b76cc337610718827ccfd3ca7d17
BilgeBengisu <76906763+BilgeBengisu@users.noreply.github.com>
2023-09-17 00:18:47 -0400

    implemented OpenAI API

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-
 import streamlit as st
-
+import openai
+import json
+#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
+openai.api_key = "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"
+def run_conversation(city, country, time):
+    # Step 1: send the conversation and available functions to GPT
+    message_string = "Hello GPT! I'm going to %s, %s for %s day(s). Can you provide me a detailed travel plan day by day? Thank you!" % (city, country, time)
+    messages = [{"role": "user", "content": message_string}]
+    #functions = []
+    response = openai.ChatCompletion.create(
+        model="gpt-4-0613",
+        messages=messages,
+        #functions=functions,
+        #function_call="auto",  # auto is default, but we'll be explicit
+    )
+    response_message = response["choices"][0]["message"]
+    return response_message
 
 st.set_page_config(page_title="Let's Travel!", page_icon="💃")
 st.header('Welcome to the :blue[Travel Planner]', divider='rainbow')
@@ -9,5 +24,12 @@ country = st.text_input('Which country are you going to?')
 city = st.text_input('Which city are you going to?')
 time = st.text_input("How long will you be there?")
 travel_button = st.button("Let's Travel!")
+if travel_button:
+    st.write('Your travel to ', city, ", ", country, " for ", time, " day(s)", model=True)
+    st.write(run_conversation(city, country, time))
 
-st.write('Your travel to ', city, ", ", country, " for ", time, "day(s)")
+
+
+
+
+
