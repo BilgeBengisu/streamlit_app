28a755eca61df256bb0524f236ac47d955b4494c
Devon Gardner <dgardner365@gmail.com>
2023-09-17 01:03:51 -0400

    Added list of countries and cities to choose from

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-
 import streamlit as st
-
+import geonamescache
 
 st.set_page_config(page_title="Let's Travel!", page_icon="💃")
 st.header('Welcome to the :blue[Travel Planner]', divider='rainbow')
 
-country = st.text_input('Which country are you going to?')
-city = st.text_input('Which city are you going to?')
-time = st.text_input("How long will you be there?")
-travel_button = st.button("Let's Travel!")
+# Generate list of countries and prompt user to select one.
+gc = geonamescache.GeonamesCache()
+countries = gc.get_countries()
+country = st.selectbox('Which country are you going to?', map(
+    lambda country: country['name'], countries.values()))
 
+# Generate list of cities based on selected country and prompt user to select
+# one.
+cities = gc.search_cities(gc.get_countries_by_names()[
+                          country]['iso'], attribute='countrycode')
+city = st.selectbox('Which city are you going to?',
+                    map(lambda city: city['name'], cities))
+
+time = st.text_input("How long will you be there (days)?")
+travel_button = st.button("Let's Travel!")
 st.write('Your travel to ', city, ", ", country, " for ", time, "day(s)")
