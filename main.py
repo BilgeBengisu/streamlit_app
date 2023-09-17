4ed72deab7bf643553c6ae40cf0c109065c3b037
Devon Gardner <dgardner365@gmail.com>
2023-09-17 01:30:07 -0400

    Added list of countries and cities to choose from

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 import streamlit as st
+import geonamescache
 import openai
 
-openai.api_key = openai_key
+openai.api_key = "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"
+
+
 def run_conversation(city, country, time):
     # Step 1: send the conversation and available functions to GPT
-    message_string = "Hello GPT! I'm going to %s, %s for %s day(s). Can you provide me a detailed travel plan day by day? Thank you!" % (city, country, time)
+    message_string = "Hello GPT! I'm going to %s, %s for %s day(s). Can you provide me a detailed travel plan day by day? Thank you!" % (
+        city, country, time)
     messages = [{"role": "user", "content": message_string}]
-    #functions = []
+    # functions = []
     response = openai.ChatCompletion.create(
         model="gpt-4-0613",
         messages=messages,
-        #functions=functions,
-        #function_call="auto",  # auto is default, but we'll be explicit
+        # functions=functions,
+        # function_call="auto",  # auto is default, but we'll be explicit
     )
     response_message = response["choices"][0]["message"]
     return response_message
 
+
 st.set_page_config(page_title="Let's Travel!", page_icon="💃")
 st.header('Welcome to the :blue[Travel Planner]', divider='rainbow')
 
-country = st.text_input('Which country are you going to?')
-city = st.text_input('Which city are you going to?')
-time = st.text_input("How long will you be there?")
+# Generate list of countries and prompt user to select one.
+gc = geonamescache.GeonamesCache()
+countries = gc.get_countries()
+country = st.selectbox('Which country are you going to?', map(
+    lambda country: country['name'], countries.values()))
+
+# Generate list of cities based on selected country and prompt user to select
+# one.
+cities = gc.search_cities(gc.get_countries_by_names()[
+                          country]['iso'], attribute='countrycode')
+city = st.selectbox('Which city are you going to?',
+                    map(lambda city: city['name'], cities))
+
+time = st.text_input("How long will you be there in days?")
 travel_button = st.button("Let's Travel!")
 if travel_button:
     st.write('Your travel to ', city, ", ", country, " for ", time, " day(s)")
     st.write(run_conversation(city, country, time))
-
-
-
-
-
-
